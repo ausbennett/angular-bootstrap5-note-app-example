54d579256d7bc5e8308851c30c2f7618ce7719b9
austinbennett <ausbenn@icloud.com>
2023-02-14 18:44:09 -0500

    Fixed new note not cascading

--- src/app/note-create/note-create.component.ts
+++ src/app/note-create/note-create.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 import {NoteService} from "../note.service";
-import {Note} from "../note.model";
 import {Router} from "@angular/router";
 
 @Component({
@@ -10,19 +9,18 @@ import {Router} from "@angular/router";
   styleUrls: ['./note-create.component.css']
 })
 export class NoteCreateComponent {
-  titleControl = new FormControl('title.', Validators.required)
-  dateControl = new FormControl('date.', Validators.required)
-  bodyControl = new FormControl('body.', Validators.required)
-
-  note:Note = {
-    title: this.titleControl.value,
-    date: this.dateControl.value,
-    body: this.bodyControl.value
-  }
+  titleControl = new FormControl('', Validators.required)
+  dateControl = new FormControl('', Validators.required)
+  bodyControl = new FormControl('', Validators.required)
 
   constructor(private noteservice:NoteService, private router:Router){}
-  submitNote(){
-    this.noteservice.addNote(this.note);
+
+submitNote(){
+    this.noteservice.addNote({
+      title: this.titleControl.value,
+      date: this.dateControl.value,
+      body: this.bodyControl.value
+    });
     this.router.navigateByUrl('/')
   }
 }
